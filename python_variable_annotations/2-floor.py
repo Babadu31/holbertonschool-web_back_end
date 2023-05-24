eff7ed0cf8ba2d5b4627c8e44e38707697a14715
Babadu31 <105010364+Babadu31@users.noreply.github.com>
2023-05-24 18:16:50 +0200

    24/05/23-18:16-2.1

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Write a type-annotated function floor which takes a float n as argument
+and returns the floor of the float.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    return a int
+    """
+    return int(n // 1)
