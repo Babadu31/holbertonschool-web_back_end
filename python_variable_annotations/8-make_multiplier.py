a9cad575a749dbd121ca0725121d3b8389f7a7e2
Babadu31 <105010364+Babadu31@users.noreply.github.com>
2023-05-24 18:56:00 +0200

    24/05/23-18:55-8.1

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+type-annotated function make_multiplier that takes a
+float multiplier as argument and returns a function
+that multiplies a float by multiplier.
+"""
+
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    takes a float multiplier as argument
+    and returns a function that multiplies a float by multiplier.
+    """
+    def multiplier_function(n: float) -> float:
+        return n * multiplier
+    return multiplier_function
+
+
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    return lambda x: x * multiplier
+"""
