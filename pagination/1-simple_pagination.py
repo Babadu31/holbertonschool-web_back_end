a6a0524aa1afa5a480688506cf1652f6f9deb298
Babadu31 <105010364+Babadu31@users.noreply.github.com>
2023-05-26 16:36:16 +0200

    Update 1-simple_pagination.py

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
+""" simple pagination with python """
 import csv
+import math
 from typing import List
 index_range = __import__("0-simple_helper_function").index_range
 
+
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """ Server class to paginate a database of popular baby names """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """ Cached dataset """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -23,13 +24,15 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert isinstance(page, int) and page > 0, "Page must be a positive integer."
-        assert isinstance(page_size, int) and page_size > 0, "Page size must be a positive integer."
-        
-        dataset = self.dataset()
-        start_index, end_index = index_range(page, page_size)
-        
-        if start_index >= len(dataset):
-            return []
+        """ takes two ints and returns a list of lists """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
-        return dataset[start_index:end_index]
+        (start, end) = index_range(page, page_size)
+        readingList = []
+
+        if end < len(self.dataset()):
+            for newPage in range(start, end):
+                readingList.append(self.dataset()[newPage])
+
+        return readingList
