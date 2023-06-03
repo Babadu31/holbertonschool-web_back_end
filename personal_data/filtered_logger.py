2db250e3dfb89e9494e8b631fc1f47779e66e51d
Babadu31 <105010364+Babadu31@users.noreply.github.com>
2023-06-03 12:22:48 +0200

    03/06/23-12:22-0.2

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
+"""Filtered logger"""
 
 import re
+import logging
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
-    return re.sub('|'.join(f'{field}=.*?{separator}' for field in fields), f'{redaction}{separator}', message)
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
+    """returns message obfuscated"""
+
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return message
