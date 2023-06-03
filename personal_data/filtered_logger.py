a7b99530888d96fedbfa0ce5459779b22a8a470c
Babadu31 <105010364+Babadu31@users.noreply.github.com>
2023-06-03 12:42:36 +0200

    03/06/23-12:42-2.1

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
-"""
-This module provides a RedactingFormatter class
-for use with Python's built-in logging module.
-This class is used to redact sensitive information,
-specified by field names, in the logging messages.
 
-This module also includes a utility function `filter_datum`
-for replacing sensitive information in text strings.
+"""
+filtered_logger.py
+Ce module fournit une classe RedactingFormatter
+pour utilisation avec le module de logging intégré de Python.
+Cette classe est utilisée pour masquer les informations sensibles,
+spécifiées par les noms de champs, dans les messages de log.
+
+Le module contient également une fonction utilitaire `filter_datum`
+pour remplacer les informations sensibles dans les chaînes de texte.
+Il contient aussi une fonction `get_logger`
+pour créer un logger avec un RedactingFormatter en tant que formateur.
+
+Enfin, il définit une constante PII_FIELDS qui contient les
+champs considérés comme des Informations Personnellement Identifiables (PII).
 """
 
 
@@ -14,11 +21,16 @@ import re
 import logging
 from typing import List
 
+# Champs à considérer comme PII
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class """
+    """
+    Classe RedactingFormatter
+    Elle étend la classe Formatter du module logging
+    pour ajouter la fonctionnalité de masquage des PII.
+    """
 
     REDACTION = "***"
     FORMAT = ("[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: "
@@ -26,12 +38,12 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        "give the lead"
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """filter values in incoming log records using filter_datum"""
+        """Filtre les valeurs dans les enregistrements
+        de log entrants en utilisant filter_datum"""
         original_format = super().format(record)
         return self.filter_datum(
             self.fields, self.REDACTION, original_format, self.SEPARATOR)
@@ -39,7 +51,25 @@ class RedactingFormatter(logging.Formatter):
     @staticmethod
     def filter_datum(fields: List[str], redaction: str, message: str,
                      separator: str) -> str:
-        """ filter out specific fields in datum """
+        """
+        Filtre des champs spécifiques dans le datum
+        """
         for field in fields:
             message = re.sub(f"{field}=[^;]+", f"{field}={redaction}", message)
         return message
+
+
+def get_logger() -> logging.Logger:
+    """
+    Renvoie un objet logging.Logger
+    Le logger a pour nom "user_data", son niveau est réglé sur INFO,
+    il n'envoie pas de messages à d'autres loggers et
+    utilise un StreamHandler avec RedactingFormatter comme formateur.
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(handler)
+    return logger
