8c1dc2bb5a874b852bab803e3ebf7769a25224c2
Babadu31 <105010364+Babadu31@users.noreply.github.com>
2023-06-03 12:58:43 +0200

    03/06/23-12:59-6.1

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -10,3 +10,21 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode(), salt)
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    This function checks if a provided password matches the hashed password.
+    Args:
+        hashed_password (bytes): The hashed password.
+        password (str): The password to verify.
+    Returns:
+        bool: True if password matches the hash, False otherwise.
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
+
+
+if __name__ == "__main__":
+    password = "MyAmazingPassw0rd"
+    hashed_password = hash_password(password)
+    print(is_valid(hashed_password, password))
