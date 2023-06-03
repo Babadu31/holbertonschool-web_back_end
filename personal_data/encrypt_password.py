f1cc6dda884db69543308ae8a7e3414f6a618ba8
Babadu31 <105010364+Babadu31@users.noreply.github.com>
2023-06-03 12:55:28 +0200

    03/06/23-12:55-5.1

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"Encrypting passwords"
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Returns a salted, hashed password, which is a byte string
+    """
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode(), salt)
+    return hashed
