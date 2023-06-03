61d490dc2551720f5b71e6b8e5e014d099c95bf2
Babadu31 <105010364+Babadu31@users.noreply.github.com>
2023-06-03 12:19:56 +0200

    1

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,4 +1,7 @@
+#!/usr/bin/env python3
+
 import re
+
 
 def filter_datum(fields, redaction, message, separator):
     return re.sub('|'.join(f'{field}=.*?{separator}' for field in fields), f'{redaction}{separator}', message)
