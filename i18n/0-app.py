75ee4f31b664a3b0e471146d991c936f1f64e7a4
Babadu31 <105010364+Babadu31@users.noreply.github.com>
2023-06-17 21:19:31 +0200

    Update 0-app.py

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,10 +1,16 @@
+#!/usr/bin/env python3
+"""basic flask app"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    """index for flask app"""
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run()
