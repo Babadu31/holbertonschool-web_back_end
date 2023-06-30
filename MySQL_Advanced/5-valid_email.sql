b0126cc77b908d8bc691d5c140813b2ed0396665
Babadu31 <105010364+Babadu31@users.noreply.github.com>
2023-06-30 13:51:03 +0200

    Update 5-valid_email.sql

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -1,8 +1,13 @@
--- resets valid email table when email table has been changed
+-- creates trigger to reset valid_email when email is changed
 
-CREATE TRIGGER email_validator BEFORE UPDATE on users
-    BEGIN
-        IF NEW.email != OLDemail THEN
-            SET NEW.valid_email = 0;
-        END IF;
-    END;
+DELIMITER //
+
+CREATE TRIGGER VALIDATE_EMAIL BEFORE UPDATE ON USERS
+FOR EACH ROW BEGIN
+	IF OLD.email != NEW.email THEN SET NEW.valid_email = 0;
+	END IF;
+END;
+
+//
+
+DELIMITER ;
