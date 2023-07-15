b41d7d308bce48080e840727b7d84b87b63317cd
Babadu31 <105010364+Babadu31@users.noreply.github.com>
2023-07-15 19:17:34 +0200

    io

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,18 +2,18 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(
-    firstName,
-    lastName,
-    fileName,
+  firstName,
+  lastName,
+  fileName,
 ) {
-    const stat = [];
-    await signUpUser(firstName, lastName)
-        .then(async (data) => {
-            stat.push({ status: 'fulfilled', value: data });
-            await uploadPhoto(fileName);
-        })
-        .catch((err) => {
-            stat.push({ status: 'rejected', value: err.toString() });
-        });
-    return stat;
+  const stat = [];
+  await signUpUser(firstName, lastName)
+    .then(async (data) => {
+      stat.push({ status: 'fulfilled', value: data });
+      await uploadPhoto(fileName);
+    })
+    .catch((err) => {
+      stat.push({ status: 'rejected', value: err.toString() });
+    });
+  return stat;
 }
