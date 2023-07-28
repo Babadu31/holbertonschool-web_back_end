34da322acfdf29e9581182d21398843692c8d373
Babadu31 <105010364+Babadu31@users.noreply.github.com>
2023-07-29 01:17:11 +0200

    Update 5-http.js

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,22 +1,23 @@
-const hostname = '127.0.0.1';
-const port = 1245;
-const DB = process.argv[2];
+const databaseFile = process.argv[2];
 
-const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+const app = http.createServer(async (req, res) => {
   if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    res.write('This is the list of our students\n');
-    countStudents(DB).then((result) => {
-      res.end(result.join('\n'));
-    }).catch((error) => {
-      res.end(`${error.message}`);
-    });
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    try {
+      const data = await countStudents(databaseFile);
+      res.end(`This is the list of our students\n${data}`);
+    } catch (error) {
+      res.end(error.message);
+    }
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Page not found');
   }
 });
 
-app.listen(port, hostname);
+app.listen(1245);
 
 module.exports = app;
